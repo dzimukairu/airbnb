5ce52a3ec34ec5f647ddab2b80cc36292114f7bf
Jim Kyle B. Padasas <jbpadasas@treasury.gov.ph>
2023-03-10 17:55:47 +0800

    Lorem ipsum.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,38 @@ function Footer() {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Lux</p>
+        <p>Lorem ipsum dolor</p>
+        <p>Sit amet</p>
+        <p>Consectetur adipiscing elit</p>
+        <p>Sed do eiusmod</p>
+        <p>Tempor incididunt</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p>Accessibility</p>
-        <p>This is not a real site</p>
-        <p>It's a pretty awesome clone</p>
-        <p>Referrals accepted</p>
-        <p>dzimukairu</p>
+        <p>Ut labore et dolore</p>
+        <p>Magna aliqua</p>
+        <p>Ut enim ad</p>
+        <p>Minim veniam</p>
+        <p>Quis nostrud exercitation</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p>Dzimu Kairu</p>
-        <p>Presents</p>
-        <p>Zero to Full Stack Hero</p>
-        <p>Hundreds of Students</p>
-        <p>Join Now</p>
+        <p>Ullamco laboris nisi</p>
+        <p>Aliquip ex ea</p>
+        <p>Commodo consequat</p>
+        <p>Duis aute irure</p>
+        <p>In reprehenderit</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
-        <p>Help Centre</p>
-        <p>Trust & Safety</p>
-        <p>Say Hi Youtube</p>
-        <p>Easter Eggs</p>
-        <p>For the Win</p>
+        <p>Voluptate velit</p>
+        <p>Esse cillum dolore</p>
+        <p>Eiusmod tempor</p>
+        <p>Incididunt ut</p>
+        <p>Labore et dolore</p>
       </div>
     </div>
   );
